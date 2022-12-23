d21a3151bced5a0ec301599bb3d4916354fc191f
Stefan <stefan@mechanicus>
2022-12-23 17:40:54 -0500

    finalized desktop changes to Polybar and i3

---
+++ software.sh
@@ -0,0 +1,22 @@
+# Free and non-free Fusion repos
+
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Flameshot
+dnf install flameshot
+
+# Polybar
+dnf install polybar
+
+# i3lock
+dnf install i3lock
+
+# vim
+dnf install vim
+
+# VLC Media Player
+dnf install vlc
+
+# Feh (Background image)
+dnf install feh
