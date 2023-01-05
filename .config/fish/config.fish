742b0f34b22cfbb8ae5f8edc42f8c6eaced6c6e3
Stefan <stefan@CFL-STOMOVICH>
2023-01-05 14:43:08 -0500

    initial commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,9 +12,13 @@ if status is-interactive
 end
 
 # Computer Control
-# Takes screenshot of desktop, blurs it, and locks screen with it as lock screen
 alias lock="scrot /tmp/lock_screen.png -o; convert -scale 10% -blur 0x2 -resize 1000% /tmp/lock_screen.png /tmp/lock_screen.png; i3lock -i /tmp/lock_screen.png -f"
 alias pcsleep="lock && systemctl suspend -i"
+
+# Multi Monitor
+alias clone-monitor-1440p="xrandr --fb 2560x1440 --output eDP-1 --mode 1920x1080 --scale-from 2560x1440 --output HDMI-2 --mode 2560x1440 --scale 1x1 --same-as eDP-1; pkill polybar; bash ~/.config/polybar/launch.sh --forest 2>/dev/null"
+alias single-monitor="xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off; ; pkill polybar; bash ~/.config/polybar/launch.sh --forest 2>/dev/null"
+alias clone-monitor-1080p="xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal; pkill polybar; bash /home/stefan/.config/polybar/launch.sh --forest 2>/dev/null"
 
 # Help Commands
 alias help-i3="open /home/stefan/Pictures/i3help.png"
